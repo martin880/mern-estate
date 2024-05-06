87903a995197249c685b83b399f97ebb40653bfb
Martin Halomoan Lumbangaol <62680881+martin880@users.noreply.github.com>
2024-05-06 11:30:13 +0700

    Update : Create Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import About from "./pages/About";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
