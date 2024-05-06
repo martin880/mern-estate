8c7afdcfb8dd3515334b9835a6d2181bf7c9a330
Martin Halomoan Lumbangaol <62680881+martin880@users.noreply.github.com>
2024-05-06 21:18:31 +0700

    Update : Create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose
@@ -16,8 +17,11 @@ mongoose
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
