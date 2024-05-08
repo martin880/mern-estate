a0a130602f2b85b780dc4d6a206c926e37b1f8a6
Martin Halomoan Lumbangaol <62680881+martin880@users.noreply.github.com>
2024-05-08 12:24:03 +0700

    Update : Complete sign in page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -21,7 +21,7 @@ export const signin = async (req, res, next) => {
     const validUser = await User.findOne({ email });
     if (!validUser) return next(errorHandler(404, "User not found"));
     const validPassword = bcryptjs.compareSync(password, validUser.password);
-    if (!validPassword) return next(errorHandler(401, "Invalid password"));
+    if (!validPassword) return next(errorHandler(401, "Invalid password !!!"));
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     // to prevent password show up from response
     const { password: pass, ...rest } = validUser._doc;
