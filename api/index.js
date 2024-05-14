b410c701f0245fc11c9811840ea9108206b716d9
Martin Halomoan Lumbangaol <62680881+martin880@users.noreply.github.com>
2024-05-14 11:41:52 +0700

    Update : user route api and user api functionality

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 mongoose
@@ -18,6 +19,7 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
